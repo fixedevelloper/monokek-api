8590b27435b58c318dd37adf45a3c02ff2fd1079
fixedevelloper <fixedevelloper@gmail.com>
2026-07-18 11:44:32 +0100

    plo

--- app/Events/PrintJobCreated.php
+++ app/Events/PrintJobCreated.php
@@ -19,13 +19,13 @@ class PrintJobCreated implements ShouldBroadcast
     public function __construct(PrintQueue $job)
     {
         // On charge la relation printer pour connaître la branche
-        $this->job = $job->load('printer');
+       // $this->job = $job->load('printer');
     }
 
     public function broadcastOn()
     {
         // On diffuse sur le canal de la branche (ex: branch.1)
-        return new Channel('branch.' . $this->job->printer->branch_id);
+      //  return new Channel('branch.' . $this->job->printer->branch_id);
     }
 
     public function broadcastAs()
